13dec143c4aad17e9dc37304029634df63e14155
DanBingZhu <daz054@ucsd.edu>
2017-08-01 13:29:59 -0700

    standarized I/O for .py

--- depthExtraction/get_time_stamp.py
+++ depthExtraction/get_time_stamp.py
@@ -1,14 +1,27 @@
+import argparse
 import rosbag
 import sys
 import os
-bag = rosbag.Bag('kinectv1_2017-06-08-15-35-13.bag')
 
-timestamps = []
+def get_time_stamp(bag_file, timestamp_file):
+	bag = rosbag.Bag(bag_file)
+	timestamps = []
 
-for topic, msg, t in bag.read_messages(topics=['/camera/depth_registered/image_raw']):
-    timestamps.append("%f\n"%t.to_sec())
+	for topic, msg, t in bag.read_messages(topics=['/camera/depth_registered/image_raw']):
+		timestamps.append("%f\n"%t.to_sec())
 
-file = open("timestamp.txt", "w")
-file.write("".join(timestamps))
-file.close()
-bag.close()
+	file = open(timestamp_file, "w")
+	file.write("".join(timestamps))
+	file.close()
+	bag.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='''
+    This script extracts timestamps from a ros bag file.
+    ''')
+
+    parser.add_argument('bag_file', help='input bag file (format: bag)')
+    parser.add_argument('timestamp_file', help='output timestamp file (format: txt)')
+    args = parser.parse_args()
+
+    get_time_stamp(args.bag_file,args.timestamp_file)
